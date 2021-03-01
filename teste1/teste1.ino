638ca3eff50e6fd09b7eda04098a407e88601237
guilherme <guilhermead@ua.pt>
2021-03-01 23:09:16 +0000

    Update teste1.ino

--- teste1/teste1.ino
+++ teste1/teste1.ino
@@ -2,19 +2,33 @@
 #include <Arduino.h>
 
 
+#define LED_PIN 5
 
+hw_timer_t * timer = NULL;
+portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
 
-void gotosleep() {
-  Serial.println("zzzz");
-  esp_sleep_enable_ext0_wakeup(GPIO_NUM_34, 0);
-  esp_deep_sleep_start();
-  
+// Code with critica section
+void IRAM_ATTR onTime() {
+  portENTER_CRITICAL_ISR(&timerMux);
+  digitalWrite(LED_PIN, HIGH);
+  portEXIT_CRITICAL_ISR(&timerMux);
 }
-
 void setup() {
   Serial.begin(115200);
-  delay(5000);
-  gotosleep();
+  esp_sleep_enable_ext0_wakeup(GPIO_NUM_34, 0);
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);
+
+  // Configure the Prescaler at 80 the quarter of the ESP32 is cadence at 80Mhz
+  // 80000000 / 800 = 100000 tics / seconde
+  timer = timerBegin(0, 800, true);
+  timerAttachInterrupt(timer, &onTime, true);
+
+  // Sets an alarm to sound every 10 seconds
+  timerAlarmWrite(timer, 1000000, true);
+  timerAlarmEnable(timer);
+  Serial.print("ZZZZ");
+  esp_deep_sleep_start();
 }
 
 void loop() {
