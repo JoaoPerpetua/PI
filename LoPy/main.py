888f3a5e4c8e085e0b7a7d90eafc4fc4e063f641
guilherme <guilhermead@ua.pt>
2021-03-11 18:35:07 +0000

    a

---
+++ LoPy/main.py
@@ -0,0 +1,31 @@
+from network import Bluetooth
+import machine
+import ubinascii
+import time
+
+bluetooth = Bluetooth()
+# Mac Address Array
+blescanmac = []
+
+while True:
+    #create mac address array
+    blescanmac.clear()
+    print("Scanning BLE devices")
+    bluetooth.start_scan(5)
+    t_end = time.time() + 6
+    while time.time() < t_end:
+        #collect adv packet from BLE scan
+        adv=bluetooth.get_adv()
+        #if there are ADV packets
+        if adv:
+            print(".")
+            #obluetoothain Mac Address
+            mac=ubinascii.hexlify(adv.mac)
+            #chekc if I already have it in Mac array
+            blescanmac.append(mac)
+
+    #Print Scan result
+    blescanmac = list(dict.fromkeys(blescanmac))
+    print("Number of detected BLE devices: "+ str(len(blescanmac)))
+    print("BLE Mac Addresses: ")
+    print(blescanmac)
