920a538222b786d7f31aac00bc06e4df17e25a4c
DiogoMMCosta <50917777+DiogoMMCosta@users.noreply.github.com>
2021-03-20 00:52:16 +0000

    Update main.py
    comunicação lora

--- LoPy JSON/main.py
+++ LoPy JSON/main.py
@@ -6,6 +6,12 @@ import machine, time
 from machine import Timer
 from network import Bluetooth
 import ubinascii
+import json
+from network import LoRa
+import socket
+
+
+
 
 #Pin Instance
 #button = Pin('P10', mode = Pin.IN)
@@ -19,9 +25,45 @@ macs = []
 active_state_pir = 0
 #Variable that defines the time needed to have no movement for the scan
 buffer_timer = 5
+
+def loracom(send):
+    # Initialise LoRa in LORAWAN mode.
+    # Europe = LoRa.EU868
+    lora = LoRa(mode=LoRa.LORAWAN, region=LoRa.EU868)
+    # create an OTAA authentication parameters, change them to the provided credentials
+    app_eui = ubinascii.unhexlify('b827ebfffeb77faa')
+    app_key = ubinascii.unhexlify('f8b476241432768a624a684a91078590')
+    # join a network using OTAA (Over the Air Activation)
+    lora.join(activation=LoRa.OTAA, auth=(app_eui, app_key), timeout=0)
+    # wait until the module has joined the network
+    while not lora.has_joined():
+        time.sleep(2.5)
+        print('Not yet joined...')
+
+    print('Joined')
+
+    # create a LoRa socket
+    s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
+
+    # set the LoRaWAN data rate
+    s.setsockopt(socket.SOL_LORA, socket.SO_DR, 5)
+    # make the socket blocking
+    # (waits for the data to be sent and for the 2 receive windows to expire)
+    s.setblocking(True)
+    # send some data
+
+    s.send(send)
+    # make the socket non-blocking
+    # (because if there's no data received it will block forever...)
+    s.setblocking(False)
+
+
+
 def scan():
     #BLE's Mac Array
     blescanmac = []
+    #BLE's json Mac Array
+    macjson = []
     #Start Scan
     bluetooth.start_scan(TIME_SCANNING)
     #Debug print
@@ -55,6 +97,13 @@ def scan():
     print("Number of detected BLE devices: "+ str(len(blescanmac)))
     print("BLE Mac Addresses: ")
     print(blescanmac)
+    macjson=jsonconvert(blescanmac)
+
+    loracom(macjson)
+
+
+
+
     time.sleep(3)
     return blescanmac
 
@@ -86,9 +135,31 @@ def time_interruption():
 
         print("\nthe racer took %f seconds to finish the race" % total)
 
+
+
+def jsonconvert(arr):
+	arr2=[]
+	l=len(arr)
+	i=0
+
+
+	while i < l:
+		y=0
+		y=arr[i].decode('utf-8')
+		t='"'+y+'"'
+		arr2.append(y)
+
+		i+=1
+
+	x= {"mac": arr2 }
+	y=json.dumps(x)
+
+
+	return y
 #led = Pin('P19', mode=Pin.OUT)
 #led.value(1)
 time_interruption()
+
 scan()
 #led.value(0)
 sleepmode()
